9ce1e97e82e05c85918d1d023928ac14ba4fdb83
Lev <markovchik2003@yandex.ru>
2026-03-02 15:55:50 +0300

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Здравствуйте, дядя Дима!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <div className="bg-image"></div>
+      <main className="content">
+        <h1>МРТ-услуги</h1>
+        <p>Обращайтесь по телефону <br/><a href="tel:89119116756">+7 (911) 911-67-56</a></p>
+      </main>
     </>
   )
 }
